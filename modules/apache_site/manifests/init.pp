3d4bf9c11b54bf16afe506d0c576d7fa7a0b1926
majedghorbel <majedgorbel@gmail.com>
2024-05-24 19:31:10 +0000

    first commit

---
+++ modules/apache_site/manifests/init.pp
@@ -0,0 +1,40 @@
+class apache_site {
+  # Ensure the git package is installed
+  package { 'git':
+    ensure => installed,
+  }
+
+  # Ensure the apache2 package is installed and the service is running
+  package { 'apache2':
+    ensure => installed,
+  }
+
+  service { 'apache2':
+    ensure     => running,
+    enable     => true,
+    subscribe  => File['/var/www/html/site'],
+  }
+
+  # Clone the HTML site from GitHub
+  exec { 'clone_site':
+    command => '/usr/bin/git clone https://github.com/majedghorbel/monsite.git /var/www/html/site',
+    #creates => '/var/www/html/site',
+    require => Package['git'],
+  }
+
+  # Ensure the /var/www/html/site directory exists and has the right permissions
+  file { '/var/www/html/site':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Exec['clone_site'],
+  }
+
+  # Ensure the Apache configuration is correct
+  file { '/etc/apache2/sites-enabled/site.conf':
+    content => template('apache_site/site.conf.erb'),
+    notify  => Service['apache2'],
+  }
+}
+
